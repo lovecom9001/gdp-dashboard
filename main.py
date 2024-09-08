b856a089bd712a1a36b58a49878d6c13f54f49b6
lovecom9001 <55788332+lovecom9001@users.noreply.github.com>
2024-09-08 23:30:10 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,6 @@ import streamlit as st
 
 st.set_page_config(page_title="ChatGPT", page_icon=".")
 st.title("ChatGPT")
+
+if user_input : = st.chat_input("메시지를 입력해 주세요."):
+    st.chat_message("user").write(f"{user_input}")
